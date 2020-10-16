20c19a7af3ca6452e4b1335af35a637c19f0403f
Abayomi Omosehin <abayomi.omosehin@venturegardengroup.com>
2020-10-16 08:09:28 -1100

    validate input

--- Program.cs
+++ Program.cs
@@ -9,10 +9,20 @@ namespace TRANSACTIONSURCHARGE
         static void Main(string[] args)
         {
             Console.WriteLine("Kindly input the amount to transfer:");
-            var inputedAmount = Convert.ToInt64(Console.ReadLine());
-            var calculateTransaction = new ReadTransactionFile(inputedAmount);
-            var result = calculateTransaction.CalculateFee();
-            Console.WriteLine($"Amount {"\t"} {"\t"} Transfer Amount {"\t"}  Charge  {"\t"} Debit Amount {"\n"} N{String.Format("{0:n}", result.TotalTransfer)} {"\t"} N{String.Format("{0:n}", result.TransferAmount)} {"\t"} {"\t"} N{String.Format("{0:n}",  result.TransferCharge)} {"\t"} N{String.Format("{0:n}", result.DebitAmount)}" );
+            string res = Console.ReadLine();
+            long value;
+            if (long.TryParse(res, out value))
+            {
+                var inputedAmount = value;
+                var calculateTransaction = new ReadTransactionFile(inputedAmount);
+                var result = calculateTransaction.CalculateFee();
+                Console.WriteLine($"Amount {"\t"} {"\t"} Transfer Amount {"\t"}  Charge  {"\t"} Debit Amount {"\n"} N{String.Format("{0:n}", result.TotalTransfer)} {"\t"} N{String.Format("{0:n}", result.TransferAmount)} {"\t"} {"\t"} N{String.Format("{0:n}", result.TransferCharge)} {"\t"} N{String.Format("{0:n}", result.DebitAmount)}");
+            }
+            else
+            {
+                Console.WriteLine("Only numbers is allowed");
+            }
+           
         }
     }
 }
