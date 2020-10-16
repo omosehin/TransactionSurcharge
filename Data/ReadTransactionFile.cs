19f61b373f1ad494f1d7e4df74fbf67b7715df44
Abayomi Omosehin <abayomi.omosehin@venturegardengroup.com>
2020-10-16 09:44:02 -1100

    separate methods

--- Data/ReadTransactionFile.cs
+++ Data/ReadTransactionFile.cs
@@ -15,16 +15,22 @@ namespace TRANSACTIONSURCHARGE
         public TransferDetails CalculateFee()
         {
             string path = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Data\\transactionFile.Json");
-            int charge = 0;
-            long TotalAmount = 0;
-            long TransferAmount = 0;
+            
             //var fileJson = File.ReadAllText(@"C:\Users\ayobami.omosehin\Documents\TRANSACTIONSURCHARGE\Data\transactionFile.Json");
             string relativePath = "..\\Data\\transactionFile.Json";
             string fullPathToFirstFile = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(relativePath), "transactionFile.Json"));
             string filePath = fullPathToFirstFile.Replace("\\bin\\Debug", "");
             var fileJson = File.ReadAllText(filePath);
             var readFile = JsonConvert.DeserializeObject<List<TransferFee>>(fileJson);
+            
+           return getAmountDetails(readFile);
+        }
 
+        private TransferDetails getAmountDetails(List<TransferFee> readFile)
+        {
+            int charge = 0;
+            long TotalAmount = 0;
+            long TransferAmount = 0;
             foreach (var item in readFile)
             {
                 if (_amount >= item.MinAmount && _amount <= item.MaxAmount)
@@ -32,16 +38,15 @@ namespace TRANSACTIONSURCHARGE
                     TotalAmount = _amount;
                     charge = item.FeeAmount;
                     TransferAmount = _amount - item.FeeAmount;
-
-
                 }
             }
-            return new TransferDetails {
+            return new TransferDetails
+            {
                 DebitAmount = TotalAmount,
                 TransferAmount = TransferAmount,
-                TransferCharge = charge, 
-                TotalTransfer = TotalAmount };
+                TransferCharge = charge,
+                TotalTransfer = TotalAmount
+            };
         }
-       
     }
 }
